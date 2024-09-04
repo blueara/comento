8482891df9ed249e2a8599457de0efb65b402c71
Dongyeong Kim <userset301@gmail.com>
2024-09-04 22:42:46 +0900

    calculator history

--- calculator/asset/js/script.js
+++ calculator/asset/js/script.js
@@ -4,6 +4,7 @@
 // '=' 버튼을 누르면, 현재 까지의 연산 결과가 화면에 표시됩니다.
 // 'C' 또는 'CE' 버튼을 통해 입력된 내용을 초기화할 수 있습니다.
 // 본인만의 추가 기능을 1개 구현합니다. (예: 기존의 연산 기록을 저장하고 볼 수 있는 기능)
+let counter = 0;
 function press(value) {
   document.getElementById("display").value += value;
 }
@@ -14,8 +15,18 @@ function clearDisplay() {
 
 function calculate() {
   try {
-    const result = eval(document.getElementById("display").value);
-    document.getElementById("display").value = result;
+    const display = document.getElementById("display");
+    const expression = display.value; // 현재 수식 저장
+    const result = eval(expression); // 수식 계산
+    counter += 1;
+    display.value = result; // 결과 표시
+
+    // 기록 추가
+
+    const resultBox = document.querySelector("#history");
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `${counter}번째 계산 : ${expression} = ${result}`; // 수식과 결과를 함께 추가
+    resultBox.appendChild(newDiv); // 새 div 요소를 기록 영역에 추가
   } catch {
     document.getElementById("display").value = "Error";
   }
